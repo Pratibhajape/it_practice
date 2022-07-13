8670989124ef4443c77c507b6467769b2fc91859
pratibhajape <pratibhavjape@gmail.com>
2022-07-13 07:44:20 +0530

    file added successfully

--- It_Practice/week2/day 2/task 4/main.js
+++ It_Practice/week2/day 2/task 4/main.js
@@ -3,3 +3,6 @@ let selection = document.querySelector('select');
 selection.addEventListener('change',()=>{
     result.innertext=selection.options[selection.selectedIndex].text;
 });
+    
+var str = document.getElementByID('Registration form').value;
+console.log(str);
